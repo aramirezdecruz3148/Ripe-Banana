e64c3eee85ffcca802d5c51fdbab3df384813d9c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-17 14:35:30 -0700

    I started with creating my studio schema and created a get route to return all studios with a passing test

--- test/studio.test.js
+++ test/studio.test.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-
 const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Studio = require('../lib/models/Studio');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -16,5 +16,33 @@ describe('app routes', () => {
 
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('can get all studios', async() => {
+    const studio = await Studio.create([{
+      name: 'StudioA',
+      address: {
+        city: 'Portland',
+        state: 'Oregon',
+        country: 'USA'
+      }
+    },
+    {
+      name: 'StudioB',
+      address: {
+        city: 'Bend',
+        state: 'Oregon',
+        country: 'USA'
+      },
+    }]);
+
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        const studiosJSON = JSON.parse(JSON.stringify(studio));
+        studiosJSON.forEach(studio => {
+          expect(res.body).toContainEqual(studio);
+        });
+      });
   });
 });
