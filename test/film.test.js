f6f57e9314cd6db3c00312369c059f147808311d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-18 08:26:37 -0700

    I have created my film schema, added the path in app, and began creating my test and the routes

---
+++ test/film.test.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const Film = require('../lib/models/Film');
+
+describe('film routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can create a film', () => {
+  
+  });
+});
