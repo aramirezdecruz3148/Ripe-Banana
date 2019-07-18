074f1397a82cf68e63c08f3d17b4b04bbd0cafa2
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-17 17:39:27 -0700

    I created a get route to return all reviewers with a passing test

--- test/reviewer.test.js
+++ test/reviewer.test.js
@@ -18,7 +18,41 @@ describe('reviewer routes', () => {
     return mongoose.connection.close();
   });
 
-  it('', () => {
-  
+  it('can create a reviewer', () => {
+    return request(app)
+      .post('/api/v1/reviewers')
+      .send({
+        name: 'Harry Potter',
+        company: 'Ministry of Magic'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Harry Potter',
+          company: 'Ministry of Magic',
+          __v: 0
+        });
+      });
+  });
+
+  it('can get all reviewers', async() => {
+    const reviewer = await Reviewer.create([{
+      name: 'Harry Potter',
+      company: 'Ministry of Magic'
+    },
+    {
+      name: 'Dirt',
+      company: 'Talk too much'
+    }]);
+
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        const reviewersJSON = JSON.parse(JSON.stringify(reviewer));
+        reviewersJSON.forEach(reviewer => {
+          expect(res.body).toContainEqual({ name: reviewer.name, _id: reviewer._id, company: reviewer.company });
+        });
+      });
   });
 });
+
