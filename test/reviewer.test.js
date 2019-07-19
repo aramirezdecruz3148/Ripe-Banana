c971c9a5c785b6eeef192636f30f85e1a0a43e9e
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-18 23:34:51 -0700

    I just created a static method for my review to return the reviews when getting by id, added the method to my route and ammended my test which is now passing

--- test/reviewer.test.js
+++ test/reviewer.test.js
@@ -4,6 +4,10 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Reviewer = require('../lib/models/Reviewer');
+const Review = require('../lib/models/Review');
+const Film = require('../lib/models/Film');
+const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 
 describe('reviewer routes', () => {
   beforeAll(() => {
@@ -61,13 +65,54 @@ describe('reviewer routes', () => {
       company: 'Ministry of Magic'
     });
 
+    const studio = await Studio.create({
+      name: 'StudioA',
+      address: {
+        city: 'Portland',
+        state: 'Oregon',
+        country: 'USA'
+      }
+    });
+
+    const actor = await Actor.create({
+      name: 'Alex',
+      dob: '1988-03-14T00:00:00.000Z',
+      pob: 'Ventura, CA'
+    });
+
+    const film = await Film.create({
+      title: 'Coolest Movie',
+      studio: studio._id,
+      released: 2017,
+      cast: [{
+        role: 'someone cool',
+        actor: actor._id
+      }]
+    }); 
+
+    const review = await Review.create({
+      rating: 4,
+      reviewer: reviewer._id,
+      review: 'this is a review',
+      film: film._id,
+    });
+
     return request(app)
       .get(`/api/v1/reviewers/${reviewer._id}`)
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'Harry Potter',
-          company: 'Ministry of Magic'
+          company: 'Ministry of Magic',
+          reviews: [{
+            _id: review._id.toString(),
+            rating: review.rating,
+            review: review.review,
+            film: { 
+              _id: film._id.toString(), 
+              title: film.title 
+            }
+          }]
         });
       });
   });
