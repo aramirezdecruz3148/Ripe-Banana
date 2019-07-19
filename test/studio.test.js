cfef66fd14d355cf8d675bc4e97ee2be496370d4
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-18 17:12:55 -0700

    I FUCKING SUCCESSFULLY WROTE MY STATIC METHOD FOR MY STUDIO AND HAVE A NEW PASSING TEST

--- test/studio.test.js
+++ test/studio.test.js
@@ -4,6 +4,8 @@ const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
 
 describe('studio routes', () => {
   beforeAll(() => {
@@ -81,9 +83,26 @@ describe('studio routes', () => {
       }
     });
 
+    const actor = await Actor.create({
+      name: 'Lava',
+      dob: '1974-04-24T00:00:00.000Z',
+      pob: 'Ventura, CA'
+    });
+
+    const film = await Film.create({
+      title: 'Coolest Movie',
+      studio: studio._id,
+      released: 2017,
+      cast: [{
+        role: 'someone cool',
+        actor: actor._id
+      }]
+    });
+
     return request(app)
       .get(`/api/v1/studios/${studio._id}`)
       .then(res => {
+        console.log(res.body);
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'StudioA',
@@ -91,7 +110,8 @@ describe('studio routes', () => {
             city: 'Portland',
             state: 'Oregon',
             country: 'USA'
-          }
+          },
+          films: [{ _id: film.id, title: film.title }]
         });
       });
   });
