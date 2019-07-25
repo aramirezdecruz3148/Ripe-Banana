a54adcdfccc4d5d532582b52bcb1973683992340
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-25 16:28:15 -0700

    created a seed-data file

---
+++ test/seed-data.js
@@ -0,0 +1,56 @@
+const Actor = require('../lib/models/Actor');
+const Film = require('../lib/models/Film');
+const Review = require('../lib/models/Review');
+const Reviewer = require('../lib/models/Reviewer');
+const Studio = require('../lib/models/Studio');
+const chance = require('chance').Chance();
+
+module.exports = async({ actors = 50, films = 100, reviews = 100, reviewers = 50, studios = 25 } = { actors: 50, films: 100, reviews: 100, reviewers: 50, studios: 25 }) => {
+  const createdActors = await Actor.create(
+    [...Array(actors)].map(() => ({
+      name: chance.name(),
+      dob: chance.date(),
+      pob: chance.state()
+    }))
+  );
+
+  const createdFilms = await Film.create(
+    [...Array(films)].map(() => ({
+      title: chance.title(),
+      studio: chance.pickone(createdStudios)._id,
+      released: chance.date(),
+      cast: [{ role: chance.name(), actor: chance.pickone(createdActors) }, { role: chance.name(), actor: chance.pickone(createdActors) }, { role: chance.name(), actor: chance.pickone(createdActors) }]
+    }))   
+  );
+
+  const createdReviews = await Review.create(
+    [...Array(reviews)].map(() => ({
+      rating: chance.prime({ min: 1, max: 5 }),
+      reviewer: chance.pickone(createdReviewers),
+      review: chance.pickone(createdReviews),
+      film: chance.pickone(createdFilms)
+    }))
+  );
+
+  const createdReviewers = await Reviewer.create(
+    [...Array(reviewers)].map(() => ({
+      name: chance.name(),
+      company: chance.company()
+    }))
+  );
+
+  const createdStudios = await Studio.create(
+    [...Array(studios)].map(() => ({
+      name: chance.name(),
+      address: { city: chance.city(), state: chance.state(), country: chance.country() }
+    }))
+  );
+
+  return {
+    actors: createdActors,
+    films: createdFilms,
+    reviews: createdReviews,
+    reviewers: createdReviewers,
+    studios: createdStudios
+  };
+};
