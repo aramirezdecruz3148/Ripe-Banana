f8fc58879b8af623b0f2b1fde277871cc554c7f6
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-26 09:29:53 -0700

    created another aggregation to return the 15 actors who have been in the most films, plugged it in and have a passing test

--- test/aggregations.test.js
+++ test/aggregations.test.js
@@ -11,4 +11,13 @@ describe('aggregate tests', () => {
         expect(res.body.length).toEqual(10);
       });
   });
+
+  it('can return the 15 actors who are in the most films', () => {
+    return request(app)
+      .get('/api/v1/aggregations/top-actors')
+      .then(res => {
+        expect(res.body.length).toEqual(15);
+        expect(res.body[0].films).toBeGreaterThan(res.body[10].films);
+      });
+  });
 });
