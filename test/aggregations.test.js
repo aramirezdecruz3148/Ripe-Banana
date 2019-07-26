84a9b97c5da7a9e4d896fd4c005defad2bfcf77c
aramirezdecruz3148 <leigh3148@gmail.com>
2019-07-26 08:08:39 -0700

    I created an agg route to return the top 10 average review rated films, plugged it in, and have a passing test

--- test/aggregations.test.js
+++ test/aggregations.test.js
@@ -1,10 +1,14 @@
-const { getActors, getFilms, getStudios, getReviews, getReviewers } = require('./data-helper');
+const { getFilms } = require('./data-helper');
 const app = require('../lib/app');
 const request = require('supertest');
 
 describe('aggregate tests', () => {
-  it('can return a list of the top 10 reviewed films', () => {
+  it('can return the top 10 highest average rated films', () => {
+    getFilms();
     return request(app)
-      .get('/api/v1/aggregations/popular-films')
+      .get('/api/v1/aggregations/top-rated')
+      .then(res => {
+        expect(res.body.length).toEqual(10);
+      });
   });
 });
